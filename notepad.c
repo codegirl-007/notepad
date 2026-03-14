c595cb0366160aafdc5117c9a323b3bfa6e4a346
codegirl-007 <s.raide@gmail.com>
2026-03-14 08:53:38 -0700

    how to make a scroll view and put text view in it

--- notepad.c
+++ notepad.c
@@ -1,15 +1,24 @@
-#include "gio/gio.h"
-#include "glib-object.h"
 #include <gtk/gtk.h>
 
 static void activate(GtkApplication *app, gpointer user_data) {
   (void)user_data;
   GtkWidget *window; 
+  GtkWidget *text_view;
+  GtkWidget *scroll;
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "minimal notepad");
   gtk_window_set_default_size(GTK_WINDOW(window), 800, 600); 
+
+  text_view = gtk_text_view_new();
+  
+  scroll = gtk_scrolled_window_new();
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scroll), text_view);
+
+  gtk_window_set_child(GTK_WINDOW(window), scroll);
+
   gtk_window_present(GTK_WINDOW(window));
+
 }
 
 int main(int argc, char **argv) {
